e7ba4e427c186eae33239c5d4de3ef40fe3791c5
Zaur Kurbanismailov <kurbanismailov.z@azurgames.com>
2026-02-13 23:44:01 +0300

    Make example work

--- Assets/Scripts/AABBUtility.cs
+++ Assets/Scripts/AABBUtility.cs
@@ -10,5 +10,87 @@ namespace DefaultNamespace
                    (minA.y <= maxB.y && maxA.y >= minB.y) &&
                    (minA.z <= maxB.z && maxA.z >= minB.z);
         }
+
+        public static HitInfo IntersectMovingAABB(Vector3 aMin, Vector3 aMax, Vector3 bMin, Vector3 bMax, Vector3 lineA, Vector3 lineB)
+        {
+            var result = new HitInfo
+            {
+                wasHit = false,
+                tEnter = 0f,
+                point = Vector3.zero,
+                normal = Vector3.zero
+            };
+
+            var d = lineB - lineA;
+
+            var tEnter = 0f;
+            var tExit = 1f;
+
+            var enterNormal = Vector3.zero;
+
+            if (!AxisCheck( aMin.x, aMax.x, bMin.x, bMax.x, d.x, Vector3.right, ref tEnter, ref tExit, ref enterNormal))
+                return result;
+
+            if (!AxisCheck(aMin.y, aMax.y, bMin.y, bMax.y, d.y, Vector3.up, ref tEnter, ref tExit, ref enterNormal))
+                return result;
+
+            if (!AxisCheck( aMin.z, aMax.z, bMin.z, bMax.z, d.z, Vector3.forward, ref tEnter, ref tExit, ref enterNormal))
+                return result;
+
+            if (tEnter < 0f || tEnter > 1f)
+                return result;
+
+            // Есть столкновение.
+            result.wasHit = true;
+            result.tEnter = tEnter;
+
+            var contactCenter = lineA + d * tEnter;
+
+            // Точка контакта (ближайшая точка A к B в момент входа)
+            var aCenterOffset = (aMin + aMax) * 0.5f - lineA;
+            result.point = contactCenter + aCenterOffset;
+
+            result.normal = enterNormal;
+
+            return result;
+        }
+
+        static bool AxisCheck(float aMin, float aMax, float bMin, float bMax, float d, Vector3 axisNormal, ref float tEnter, ref float tExit, ref Vector3 enterNormal)
+        {
+            if (Mathf.Approximately(d, 0f))
+            {
+                if (aMax < bMin || aMin > bMax)
+                    return false;
+
+                return true;
+            }
+
+            var t1 = (bMin - aMax) / d;
+            var t2 = (bMax - aMin) / d;
+
+            var enter = Mathf.Min(t1, t2);
+            var exit = Mathf.Max(t1, t2);
+
+            if (enter > tEnter)
+            {
+                tEnter = enter;
+                enterNormal = (t1 < t2) ? -axisNormal : axisNormal;
+            }
+
+            tExit = Mathf.Min(tExit, exit);
+
+            if (tEnter > tExit)
+                return false;
+
+            return true;
+        }
+
+        public struct HitInfo
+        {
+            public bool wasHit;
+            public float tEnter;
+            public Vector3 point;
+            public Vector3 normal;
+        }
     }
 }
--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -8,12 +8,57 @@ public class Example : MonoBehaviour
 {
     [SerializeField] private MeshRenderer _a;
     [SerializeField] private MeshRenderer _b;
+    [SerializeField] private Transform _lineA;
+    [SerializeField] private Transform _lineB;
     
     private void Update()
     {
         var aBounds = _a.bounds;
         var bBounds = _b.bounds;
         
-        print(AABBUtility.Intersects(aBounds.min, aBounds.max, bBounds.min, bBounds.max));
+        // print(AABBUtility.Intersects(aBounds.min, aBounds.max, bBounds.min, bBounds.max));
+        var result = AABBUtility.IntersectMovingAABB(aBounds.min, aBounds.max, bBounds.min, bBounds.max, _lineA.position, _lineB.position);
+
+        Debug.DrawLine(_lineA.position, _lineB.position, Color.yellow);
+        
+        if (!result.wasHit)
+            print(false);
+        else
+        {
+            print($"true {result.tEnter} {result.point}");
+            var intersectBounds = new Bounds(result.point, aBounds.size);
+            DrawWireAABB(intersectBounds.min, intersectBounds.max);
+        }
     }
+
+    private void DrawWireAABB(Vector3 min, Vector3 max)
+    {
+        var p = new Vector3[8];
+
+        p[0] = new Vector3(min.x, min.y, min.z);
+        p[1] = new Vector3(max.x, min.y, min.z);
+        p[2] = new Vector3(max.x, max.y, min.z);
+        p[3] = new Vector3(min.x, max.y, min.z);
+
+        p[4] = new Vector3(min.x, min.y, max.z);
+        p[5] = new Vector3(max.x, min.y, max.z);
+        p[6] = new Vector3(max.x, max.y, max.z);
+        p[7] = new Vector3(min.x, max.y, max.z);
+
+        Debug.DrawLine(p[0], p[1], Color.yellow);
+        Debug.DrawLine(p[1], p[2], Color.yellow);
+        Debug.DrawLine(p[2], p[3], Color.yellow);
+        Debug.DrawLine(p[3], p[0], Color.yellow);
+
+        Debug.DrawLine(p[4], p[5], Color.yellow);
+        Debug.DrawLine(p[5], p[6], Color.yellow);
+        Debug.DrawLine(p[6], p[7], Color.yellow);
+        Debug.DrawLine(p[7], p[4], Color.yellow);
+
+        Debug.DrawLine(p[0], p[4], Color.yellow);
+        Debug.DrawLine(p[1], p[5], Color.yellow);
+        Debug.DrawLine(p[2], p[6], Color.yellow);
+        Debug.DrawLine(p[3], p[7], Color.yellow);
+    }
+
 }
